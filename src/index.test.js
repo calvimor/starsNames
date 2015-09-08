6b008ccf5da9fb91024d10393c405b88f1ec4295
Carlos Alberto Villamizar Morantes <calvimor@hotmail.com>
2015-09-08 16:46:20 -0500

    Corregida lista de nombre faltaba una coma de separación y se agrega funcionalidad de testing con mocha

---
+++ src/index.test.js
@@ -0,0 +1,9 @@
+var expect = require('chai').expect;
+var starWars = require('./index');
+
+
+describe('starwars-names-cavm', function() {
+    it('should work!', function() {
+        expect(true).to.be.true;
+    });
+});
